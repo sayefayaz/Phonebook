20d81fa837883eb5e72b2bdedcb9a60cef12a022
sayefayaz <sayeefayaz@gmail.com>
2023-12-21 17:59:30 +0330

    saye

--- src/test/java/Phonebook_Test.java
+++ src/test/java/Phonebook_Test.java
@@ -11,14 +11,26 @@ public class Phonebook_Test {
         phonebook.addContact(person2);
     }
     @Test
-    public void getPhoneNumber_test()
+    public void getPhoneNumber_test() {
+        Phonebook phonebook = new Phonebook();
+        Person person1 = new Person("saye", "09055524298");
+        Person person2 = new Person("maryam", "09912650261");
+        phonebook.addContact(person1);
+        phonebook.addContact(person2);
+        assertEquals("09055524298", phonebook.getPhoneNumber("saye"));
+        assertEquals("09912650261", phonebook.getPhoneNumber("maryam"));
+    }
+    @Test
+    public void getContact_test()
     {
         Phonebook phonebook=new Phonebook();
         Person person1=new Person("saye","09055524298");
         Person person2=new Person("maryam","09912650261");
         phonebook.addContact(person1);
         phonebook.addContact(person2);
-        assertEquals("09055524298",phonebook.getPhoneNumber("saye"));
-        assertEquals("09912650261",phonebook.getPhoneNumber("maryam"));
+        assertEquals("saye",phonebook.getContact("09055524298"));
+        assertEquals("maryam",phonebook.getContact("09912650261"));
+
     }
+
 }
